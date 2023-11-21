15026c988bfc76fbf3d3357f32a1ed1f9beb52c7
BOOK-119KMIQA3U\kmh03 <kmh030877@gmail.com>
2023-11-22 04:18:04 +0900

    카페브랜치 완성

---
+++ app/src/main/java/com/example/hyoja/cafe/activity/CardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hyoja.cafe.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hyoja.R
+
+class CardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card)
+    }
+}
--- app/src/main/java/com/example/hyoja/cafe/activity/PayActivity.kt
+++ app/src/main/java/com/example/hyoja/cafe/activity/PayActivity.kt
@@ -9,22 +9,42 @@ import com.example.hyoja.cafe.model.CafeModel
 import com.example.hyoja.common.util.CommonUi
 import com.example.hyoja.databinding.ActivityPayBinding
 
-class PayActivity : AppCompatActivity() {
+class PayActivity : AppCompatActivity(), PayFragment.NextListener {
     lateinit var binding: ActivityPayBinding
-    lateinit var payViewPagerAdapter: PayViewPagerAdapter
+    private val payViewPagerAdapter = PayViewPagerAdapter(this)
+    private var currentItem = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPayBinding.inflate(layoutInflater)
         setContentView(binding.root)
         CafeModel.ActivityStatus = this
 
-        payViewPagerAdapter = PayViewPagerAdapter(this)
-
         binding.viewPager.adapter = payViewPagerAdapter
+        binding.viewPager.isUserInputEnabled = false
+        payViewPagerAdapter.setNextListener(this)
+        binding.price.text = CafeModel.getTotalPrice().toString()
+        binding.remainPrice.text = CafeModel.getTotalPrice().toString()
+
+        binding.allCancel.setOnClickListener {
+            CafeModel.drinkSelectedList.clear()
+            CommonUi().goToCafeHome1(this)
+        }
+        binding.back.setOnClickListener {
+            if (currentItem == 0){
+                this.finish()
+            } else {
+                binding.viewPager.currentItem = --currentItem
+            }
+
+        }
+        binding.next.setOnClickListener {
+            binding.viewPager.currentItem = ++currentItem
+        }
     }
 
     private var backPressedTime:Long = 0
     override fun onBackPressed() {
+        // 뒤로가기 막기
         val currentTime = System.currentTimeMillis()
         val commonUi:CommonUi = CommonUi()
         val view = this
@@ -40,5 +60,9 @@ class PayActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+    }
+
+    override fun setNextItem() {
+        binding.viewPager.setCurrentItem(2,true)
     }
 }
